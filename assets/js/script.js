b3920116c0b2e67da7fed05ab9084137194c6888
Forhad Ahmed <ahmedforhad5@gmail.com>
2022-12-11 01:18:01 +0600

    updagd

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,10 +70,31 @@ sidebarBtn.addEventListener("click", ()=>{
 });
 
 
+// Today Customer Bill list
 $(document).ready(function(){
-  $("#myInput").on("keyup", function() {
+  $("#customer-bill-list").on("keyup", function() {
     var value = $(this).val().toLowerCase();
-    $("#tableData tr").filter(function() {
+    $("#customer-bill-list-data tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+// Today Expence Total list
+$(document).ready(function(){
+  $("#today-expence-list").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#today-expence-total-data tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+// Today Bill Recived list
+$(document).ready(function(){
+  $("#today-bill-recived").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#today-bill-recived-data tr").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
